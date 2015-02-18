130a88644808f2fb61c4d0db8d8d5cc064d5557b
Sean Papay <sean.papay@gmail.com>
2015-02-18 01:29:51 -0800

    3d plots kind of render
    for some reason, only the first handful of triangles are drawn...

---
+++ js/geometry.js
@@ -0,0 +1,137 @@
+var makeCube = function(size) {
+	var l = size = 2;
+/*
+	    y
+	    |
+	    e-----f
+	   /|    /|
+	  h-----g |
+	  | a---|-b---x
+	  |/    |/
+	  d-----c
+	 /
+	z
+
+*/
+	
+	var a = [-l, -l, -l];
+	var b = [+l, -l, -l];
+	var c = [+l, -l, +l];
+	var d = [-l, -l, +l];
+	var e = [-l, +l, -l];
+	var f = [+l, +l, -l];
+	var g = [+l, +l, +l];
+	var h = [-l, +l, +l];
+	
+	var quadverts = function(a, b, c, d) {
+		return [].concat(a, b, d, b, c, d);
+	}
+	
+	var verts = [].concat(
+		quadverts(a, b, c, d),
+		quadverts(e, h, g, f),
+		quadverts(d, c, g, h),
+		quadverts(b, a, e, f),
+		quadverts(c, b, f, g),
+		quadverts(e, a, d, h)
+	);
+	return new Float32Array(verts);
+}
+
+var makeGraph = function(data) {
+				
+/*
+	.-----X--->
+	|
+	Z  a---b---+-
+	|  |\4/|\ /|\
+	|  |1e3| + |
+	V  |/2\|/ \|/ ...
+		 c---d---+-
+		 |\ /|\ /|\
+	      .
+	   	  .
+	      .
+		
+*/
+	var rows = data.length;
+	var cols = data[0].length;
+	var r = rows - 1;
+	var c = cols - 1;
+	var numVerts = r*c*12;
+	var vertArray = new Float32Array(3 * numVerts);
+	for (var i = 0; i < r; ++i) {
+		for (var j = 0; j < c; ++j) {
+			var off = 36*(r*i+j);
+			var a_x = j/c;
+			var b_x = (j+1)/c;
+			var c_x = a_x;
+			var d_x = b_x;
+			var a_z = i/r;
+			var c_z = (i+1)/r;
+			var b_z = a_z;
+			var d_z = c_z;
+			var e_x = (a_x+b_x)/2
+			var e_z = (a_z+c_z)/2;
+			var a_y = data[i][j];
+			var b_y = data[i][j+1];
+			var c_y = data[i+1][j];
+			var d_y = data[i+1][j+1];
+			var e_y = (a_y+b_y+c_y+d_y)/4;
+			
+			vertArray[off + 0] = a_x;
+			vertArray[off + 1] = a_y;
+			vertArray[off + 2] = a_z;
+			vertArray[off + 3] = c_x;
+			vertArray[off + 4] = c_y;
+			vertArray[off + 5] = c_z;
+			vertArray[off + 6] = e_x;
+			vertArray[off + 7] = e_y;
+			vertArray[off + 8] = e_z;
+			
+			vertArray[off + 9] = c_x;
+			vertArray[off +10] = c_y;
+			vertArray[off +11] = c_z;
+			vertArray[off +12] = d_x;
+			vertArray[off +13] = d_y;
+			vertArray[off +14] = d_z;
+			vertArray[off +15] = e_x;
+			vertArray[off +16] = e_y;
+			vertArray[off +17] = e_z;
+			
+			vertArray[off +18] = d_x;
+			vertArray[off +19] = d_y;
+			vertArray[off +20] = d_z;
+			vertArray[off +21] = b_x;
+			vertArray[off +22] = b_y;
+			vertArray[off +23] = b_z;
+			vertArray[off +24] = e_x;
+			vertArray[off +25] = e_y;
+			vertArray[off +26] = e_z;
+			
+			vertArray[off +27] = b_x;
+			vertArray[off +28] = b_y;
+			vertArray[off +29] = b_z;
+			vertArray[off +30] = a_x;
+			vertArray[off +31] = a_y;
+			vertArray[off +32] = a_z;
+			vertArray[off +33] = e_x;
+			vertArray[off +34] = e_y;
+			vertArray[off +35] = e_z;
+			
+		}
+	}
+	return vertArray;
+};
+
+
+
+var makeVbo = function(geometry, mode){
+	debugger;
+	mode = mode || gl.STATIC_DRAW;
+	var vbo = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+	gl.bufferData(gl.ARRAY_BUFFER, geometry.length * 4, mode);
+	gl.bufferData(gl.ARRAY_BUFFER, geometry, mode);
+	return vbo;
+}
--- js/main.js
+++ js/main.js
@@ -25,52 +25,56 @@ var scope = {
 	},
 	
 	init: function(data) {
+		glMatrix.setMatrixArrayType(Float32Array);
+		
 		scope.data = data;
 		var rows = data.length;
 		var cols = data[0].length;
-		var numVerts = scope.getNumVerts(data);
-		vertArray = new Float32Array(numVerts * 3);
-		var vbo = gl.createBuffer();
-		gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
-		gl.bufferData(gl.ARRAY_BUFFER, numVerts * vertArray.BYTES_PER_ELEMENT * 3, gl.DYNAMIC_DRAW);
 		
 		var vertSrc = document.getElementById('vertShader').text;
 		var fragSrc = document.getElementById('fragShader').text;
 		
 		var program = scope.buildShaderProgram(vertSrc, fragSrc);
 		var posIndex = gl.getAttribLocation(program, "pos");
-		gl.vertexAttribPointer(posIndex, 3, gl.FLOAT, gl.FALSE, vertArray.BYTES_PER_ELEMENT * 3, 0);
+		
+		var modelMatLoc = gl.getUniformLocation(program, 'modelMat');
+		var viewMatLoc = gl.getUniformLocation(program, 'viewMat');
+		var projMatLoc = gl.getUniformLocation(program, 'projMat');
+		
+		gl.useProgram(program);
+		var modelMat = mat4.create();
+		var viewMat = mat4.create()
+		mat4.translate(viewMat, viewMat, vec3.fromValues(0, 0, -3));
+		mat4.rotateX(viewMat, viewMat, Math.PI/2);
+		var n = 0.01, f = 1000;
+		var b = -0.001, t = 0.001;
+		var l = -0.01, r = 0.01;
+		var projMat = mat4.frustum(mat4.create(), l, r, b, t, n, f);
+		
+		
+		gl.uniformMatrix4fv(modelMatLoc, false, modelMat);
+		gl.uniformMatrix4fv(viewMatLoc, false, viewMat);
+		gl.uniformMatrix4fv(projMatLoc, false, projMat);
+		
+		gl.useProgram(program);
+		
+		var geometry = makeGraph(data);
+		var vbo = makeVbo(geometry);
+		gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+		
+		gl.vertexAttribPointer(posIndex, 3, gl.FLOAT, false,  4 * 3, 0);
 		
 		gl.enableVertexAttribArray(posIndex);
 		
-		var modelViewMatLoc = gl.getUniformLocation(program, 'modelViewMat');
-		var projMatLoc = gl.getUniformLocation(program, 'projMat');
-		gl.useProgram(program);
-		var idenMat = new Float32Array(16);
-		idenMat[0] = idenMat[5] = idenMat[10] = idenMat[15] = 1;
-		var n = 0.01, f = 1000;
-		var b = -1, t = 1;
-		var l = -1, r = 1;
-		var projMat = toFloat32Array(transpose([
-			2*n/(r-l), 0,         (r+l)/(r-l),  0,
-			0,         2*n/(t-b), (t+b)/(t-b),  0,
-			0,         0,         (-n-f)/(f-n), -2*f*n/(f-n),
-			0,         0,         -1,           0,
-		]));
-		
-		
-		gl.uniformMatrix4fv(modelViewMatLoc, gl.FALSE, idenMat);
-		gl.uniformMatrix4fv(projMatLoc, gl.FALSE, projMat);
-		
-		gl.useProgram(program);
-		
-		
-		scope.buildGraphMesh(data, vbo);
+		//var vbo = scope.makeGraph(data);
+		//gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
 		
 		gl.clearColor(0, 0, 1, 1);
 		
 		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-		gl.drawArrays(gl.TRIANGLES, 0, numVerts);
+		gl.drawArrays(gl.TRIANGLES, 0, geometry.length/3);
+		gl.finish();
+		console.log('done');
 		
 	},
 	
@@ -98,96 +102,15 @@ var scope = {
 		return program;
 	},
 	
-	
-	getNumVerts: function(data) {
-		var rows = data.length;
-		var cols = data[0].length;
-		return 12 * (rows-1) * (cols - 1);
-	},
-	/*
-	.-----X--->
-	|
-	Z  a---b---+-
-	|  |\4/|\ /|\
-	|  |1e3| + |
-	V  |/2\|/ \|/ ...
-		 c---d---+-
-		 |\ /|\ /|\
-	      .
-	   	  .
-	      .
-		
-	*/
-	buildGraphMesh: function(data, vbo) {
-		gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
-		var vertArray = new Float32Array(3 * scope.getNumVerts(data));
-		var rows = data.length;
-		var cols = data[0].length;
-		var r = rows - 1;
-		var c = cols - 1;
-		for (var i = 0; i < r; ++i) {
-			for (var j = 0; j < c; ++j) {
-				var off = 36*(r*i+j);
-				var a_x = j/c;
-				var b_x = (j+1)/c;
-				var c_x = a_x;
-				var d_x = b_x;
-				var a_z = i/r;
-				var c_z = (i+1)/r;
-				var b_z = a_z;
-				var d_z = c_z;
-				var e_x = (a_x+b_x)/2
-				var e_z = (a_z+c_z)/2;
-				var a_y = data[i][j];
-				var b_y = data[i][j+1];
-				var c_y = data[i+1][j];
-				var d_y = data[i+1][j+1];
-				var e_y = (a_y+b_y+c_y+d_y)/4;
-				
-				vertArray[off + 0] = a_x;
-				vertArray[off + 1] = a_y;
-				vertArray[off + 2] = a_z;
-				vertArray[off + 3] = c_x;
-				vertArray[off + 4] = c_y;
-				vertArray[off + 5] = c_z;
-				vertArray[off + 6] = e_x;
-				vertArray[off + 7] = e_y;
-				vertArray[off + 8] = e_z;
-				
-				vertArray[off + 9] = c_x;
-				vertArray[off +10] = c_y;
-				vertArray[off +11] = c_z;
-				vertArray[off +12] = d_x;
-				vertArray[off +13] = d_y;
-				vertArray[off +14] = d_z;
-				vertArray[off +15] = e_x;
-				vertArray[off +16] = e_y;
-				vertArray[off +17] = e_z;
-				
-				vertArray[off +18] = d_x;
-				vertArray[off +19] = d_y;
-				vertArray[off +20] = d_z;
-				vertArray[off +21] = b_x;
-				vertArray[off +22] = b_y;
-				vertArray[off +23] = b_z;
-				vertArray[off +24] = e_x;
-				vertArray[off +25] = e_y;
-				vertArray[off +26] = e_z;
-				
-				vertArray[off +27] = b_x;
-				vertArray[off +28] = b_y;
-				vertArray[off +29] = b_z;
-				vertArray[off +30] = a_x;
-				vertArray[off +31] = a_y;
-				vertArray[off +32] = a_z;
-				vertArray[off +33] = e_x;
-				vertArray[off +34] = e_y;
-				vertArray[off +35] = e_z;
-				
-			}
-		}
-		gl.bufferData(gl.ARRAY_BUFFER, vertArray.buffer, gl.DYNAMIC_DRAW);
-	},
-	
 };
 window.bciVis = scope;
+
+var zeroData = [];
+for (var i = 0; i < 100; ++i) {
+	zeroData.push([]);
+	for (var j = 0; j < 8; ++j) {
+		zeroData[i].push((i+j) % 2);
+	}
+}
+
+scope.init(zeroData);
