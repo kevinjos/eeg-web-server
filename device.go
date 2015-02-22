15defda538ebda76a2dd217325a42293615fe8bd
kjs <kjs@llama>
2015-02-22 13:50:41 -0800

    add readme and agpl license

--- device.go
+++ device.go
@@ -1,3 +1,21 @@
+/*  OpenBCI golang server allows users to control, visualize and store data
+    collected from the OpenBCI microcontroller.
+    Copyright (C) 2015  Kevin Schiesser
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package main
 
 import (
@@ -50,8 +68,8 @@ func (d *OpenBCI) command() {
 		select {
 		case s := <-d.writeChan:
 			d.write(s)
-		case resumePacketStream := <-d.resetChan:
-			go d.reset(resumePacketStream)
+		case resumePacketChan := <-d.resetChan:
+			go d.reset(resumePacketChan)
 		case <-d.quitCommand:
 			return
 		}
